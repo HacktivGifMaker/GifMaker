f507a333c24bb919208304074497bf53c452f331
arnoldherod <arnold.herod@gmail.com>
2018-12-07 00:08:43 +0700

    Initializing multer, and GCP

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('../helpers/converter')
+const {sendUploadToGCS} = require('../controllers/gyfController')
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('respond with a resource');
 });
+router.post('/upload', multer.single('userPhoto'), sendUploadToGCS)
+
 
 module.exports = router;
