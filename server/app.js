60f7160ba45e5d87c7218a1ea8f91e78efdbca04
hafrizresa <hafrizresa@gmail.com>
2018-12-07 13:08:32 +0700

    convert gif and upload gcp feature

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ var app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
